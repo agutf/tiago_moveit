4fc343c88c152ace006b90f4c3a610715c10351b
Alexis <agutf@unileon.es>
2024-07-08 13:56:35 +0200

    Moving Tiago to random pose

---
+++ src/move_tiago.cpp
@@ -0,0 +1,73 @@
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit_msgs/msg/display_robot_state.hpp>
+#include <moveit_msgs/msg/display_trajectory.hpp>
+
+#include <moveit_msgs/msg/attached_collision_object.hpp>
+#include <moveit_msgs/msg/collision_object.hpp>
+
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("move_tiago");
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions node_options;
+  node_options.automatically_declare_parameters_from_overrides(true);
+  auto move_group_node = rclcpp::Node::make_shared("move_tiago", node_options);
+
+  // We spin up a SingleThreadedExecutor for the current state monitor to get information
+  // about the robot's state.
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(move_group_node);
+  std::thread([&executor]() { executor.spin(); }).detach();
+
+  static const std::string PLANNING_GROUP = "arm";
+
+  // The
+  // :moveit_codedir:`MoveGroupInterface<moveit_ros/planning_interface/move_group_interface/include/moveit/move_group_interface/move_group_interface.h>`
+  // class can be easily set up using just the name of the planning group you would like to control and plan for.
+  moveit::planning_interface::MoveGroupInterface move_group(move_group_node, PLANNING_GROUP);
+
+  // We will use the
+  // :moveit_codedir:`PlanningSceneInterface<moveit_ros/planning_interface/planning_scene_interface/include/moveit/planning_scene_interface/planning_scene_interface.h>`
+  // class to add and remove collision objects in our "virtual world" scene
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+
+  // Raw pointers are frequently used to refer to the planning group for improved performance.
+  const moveit::core::JointModelGroup* joint_model_group =
+      move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+  // Getting Basic Information
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^
+  //
+  // We can print the name of the reference frame for this robot.
+  RCLCPP_INFO(LOGGER, "Planning frame: %s", move_group.getPlanningFrame().c_str());
+
+  // We can also print the name of the end-effector link for this group.
+  RCLCPP_INFO(LOGGER, "End effector link: %s", move_group.getEndEffectorLink().c_str());
+
+  geometry_msgs::msg::PoseStamped currentPose = move_group.getCurrentPose();
+  RCLCPP_INFO(LOGGER, "Current pose: %f %f %f", currentPose.pose.position.x, currentPose.pose.position.y, currentPose.pose.position.z);
+
+  geometry_msgs::msg::PoseStamped randomPose = move_group.getRandomPose();
+  RCLCPP_INFO(LOGGER, "Random pose: %f %f %f", randomPose.pose.position.x, randomPose.pose.position.y, randomPose.pose.position.z);
+
+
+  move_group.setPoseTarget(randomPose);
+
+  // Now, we call the planner to compute the plan and visualize it.
+  // Note that we are just planning, not asking move_group
+  // to actually move the robot.
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+  bool success = (move_group.plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
+
+  RCLCPP_INFO(LOGGER, "Plan for random pose: %s", success ? "" : "FAILED");
+
+  if(success) {
+    move_group.move();
+  }
+
+  return 0;
+}
